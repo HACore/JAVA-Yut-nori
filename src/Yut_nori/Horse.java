b741ecb54ce5a7fa80967a4b251475796d04e295
hyunjeong408 <65072995+hyunjeong408@users.noreply.github.com>
2020-06-09 20:07:55 +0900

    Update Horse.java

--- src/Yut_nori/Horse.java
+++ src/Yut_nori/Horse.java
@@ -6,38 +6,41 @@ public class Horse {
 	int getposition() {
 		return this.position;
 	}
-	void changep (int now, String s) {
+	void changep (int d, String s) {
 		/*String s는 직진할지 꺾을지 입력받는 건데 어떻게 입력받을지 모르겠어서 임의로 string으로 설정*/
-		switch(now) {
+		switch(position) {
 		case 5:
 			if(s.equals("go")) {
-				this.position = 6;
+				this.position = 6+(d-1);
 			}
 			else {
-				this.position = 20;
+				this.position = 20+(d-1);
 			}
 			break;
 		case 10:
 			if(s.equals("go")) {//밑으로 내려가는 경우
-				this.position = 11;
+				this.position = 11+(d-1);
 			}
 			else {
-				this.position = 25;
+				this.position = 25+(d-1);
 			}
 			break;
 		case 15://이건 위로 올라가는 case를 따져야할지 모르겠어서 일단 보류
-			this.position++;
+			this.position += d;
 			break;
 		case 22:
-			if(s.equals("go")) {
-				this.position = 23; //왼쪽 길로 내려갈 때
+			if(d>2) {
+				this.position = 15+(d-3);
 			}
 			else {
-				this.position = 27;
+				this.position += d;
 			}
 			break;
+		case 27:
+			this.position += d;
 		default:
-			this.position++;
+			//여기...20~24사이...case 나눠야할듯
+			this.position += d;
 		}
 	}
 }
