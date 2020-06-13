d2579d66d50835b4896384fd7939b59f03ac2b85
hyunjeong408 <65072995+hyunjeong408@users.noreply.github.com>
2020-06-14 03:59:14 +0900

    Update Horse.java

--- src/Yut_nori/Horse.java
+++ src/Yut_nori/Horse.java
@@ -1,97 +1,84 @@
 package Yut_nori;
 
 public class Horse {
+
 	private int position = -1;
+	int posx, posy;
+	int go;
+	char s;
+	public int id;//이건 혹시 명륜인지 율전인지 파악 필요하면 쓰려고 넣어뒀는데 솔직히 쓸지안쓸지는 모르겠어
 	
-	int getposition() {
+	void getBoardPos(Board board) {
+		Board b = board;
+		posx = b.pos[position].x;
+		posy = b.pos[position].y;
+	}
+	
+	void getYutMove(Yut yut) {
+		Yut y = yut;
+		go = yut.getMove();
+	}
+	
+	void getDirection (Frame f) {//어떤 방향?으로 갈지 받아오는 거
+		s = f.ReturnC();
+	}
+
+	int getposition() {//이건 혹시 말 위치 다른 곳에서 쓸 일 있으면 값 리턴해줄 함수
 		return this.position;
 	}
-	void changep (int d, String s) {
-		/*String s는 직진할지 꺾을지 입력받는 건데 어떻게 입력받을지 모르겠어서 임의로 string으로 설정*/
+	
+	void gotoStart() {//이건 걍 잡아먹히는 그런 경우?에... Game에서 실행시켜주면 안될까...?
+		this.position = -1;
+	}
+	
+	void changep () {
+
+		char left = 'l';
+		char right = 'r';
+		
 		switch(position) {
 		case 5:
-			if(s.equals("go")) {
-				this.position = 6+(d-1);
+			if(s == left) {
+				this.position = 20+(go-1);
 			}
 			else {
-				this.position = 20+(d-1);
+				this.position = 6+(go-1);
 			}
 			break;
 		case 10:
-			if(s.equals("go")) {//밑으로 내려가는 경우
-				this.position = 11+(d-1);
+			if(s == right) {//밑으로 내려가는 경우
+				this.position = 25+(go-1);
 			}
 			else {
-				this.position = 25+(d-1);
+				this.position = 11+(go-1);
 			}
 			break;
-		case 15://이건 위로 올라가는 case를 따져야할지 모르겠어서 일단 보류
-			this.position += d;
+		case 15:
+			this.position += go;
 			break;
 		case 22:
-			if(d>2) {
-				this.position = 15+(d-3);
+			if(go>2) {
+				this.position = 15+(go-3);
 			}
 			else {
-				this.position += d;
+				this.position += go;
 			}
 			break;
 		case 27:
-			this.position += d;
+			this.position += go;
 		default:
 			if(position>=20 && position<=24) {
-				if(d>=(25-position)) {
-					this.position = 15+(d-(25-position));
+				if(go>=(25-position)) {
+					this.position = 15+(go-(25-position));
 				}
 				else {
-					this.position += d;
+					this.position += go;
 				}
 			}
 			else {
-				this.position += d;
+				this.position += go;
 			}
 		}
 	}
-}
 
-class HorsePanel extends JPanel{
-	
-	ImageIcon imageM;
-	ImageIcon imageY;
-	JPanel Horses1 = new JPanel();
-	JPanel Horses2 = new JPanel();
-	JLabel p1 = new JLabel("Player1");
-	JLabel p2 = new JLabel("Player2");
-	JLabel image1 = new JLabel();
-	JLabel image2 = new JLabel();
-	
-	public HorsePanel() {
-		setLayout(null);
-		setBounds(0, 0, 1, 1);
-		
-		Horses1.setLayout(new GridLayout(1,5));
-		Horses2.setLayout(new GridLayout(1,5));
-		
-		imageM = new ImageIcon("../image/M.PNG");
-		imageY = new ImageIcon("../image/Y.PNG");
-		
-		image1 = new JLabel(imageM);
-		image2 = new JLabel(imageY);
-		
-		Horses1.add(p1);
-		Horses1.add(image1);
-		Horses1.add(image1);
-		Horses1.add(image1);
-		Horses1.add(image1);
-		
-		Horses2.add(p2);
-		Horses2.add(image2);
-		Horses2.add(image2);
-		Horses2.add(image2);
-		Horses2.add(image2);
-		
-		
-		this.add(Horses1);
-		this.add(Horses2);
-	}
 }
