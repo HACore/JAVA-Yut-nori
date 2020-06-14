9a7c528343d1b5bd3facdd19e1c91878e1a03ada
hyunjeong408 <65072995+hyunjeong408@users.noreply.github.com>
2020-06-15 00:46:38 +0900

    Update Player.java

--- src/Yut_nori/Player.java
+++ src/Yut_nori/Player.java
@@ -1,7 +1,24 @@
 package Yut_nori;
 
 public class Player {
+	int id;
 	Horse[] horse = new Horse[4];
+	{
+		for(int i = 0; i < 4; i++) {
+			horse[i] = new Horse();
+		}
+	}
+	Player(int k){
+		this.id = k;
+		for(int i = 0; i < 4; i++) {
+			if(horse[i].id == 0) {
+				horse[i].num = i;
+			}
+			else {
+				horse[i].num = 3+i;
+			}
+		}
+	}
 	int score = 0;
 	int throwing = 1;
 }
