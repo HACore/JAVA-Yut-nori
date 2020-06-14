6ff7b4e264e3f9279affe5bf4db9704ac35edef7
hyunjeong408 <65072995+hyunjeong408@users.noreply.github.com>
2020-06-15 04:45:53 +0900

    Update Yut.java

--- src/Yut_nori/Yut.java
+++ src/Yut_nori/Yut.java
@@ -1,5 +1,61 @@
 package Yut_nori;
 
-public class Yut {
+import java.util.Random;
+import java.awt.*;
+//import java.awt.event.*;
+import javax.swing.*;
 
+public class Yut {
+	private int move;
+	
+	//ButtonClickListener
+	public int Throw() {
+		
+		Random random = new Random();
+		move = random.nextInt(6);
+		return move;
+	}
+	
+	public int getMove() {
+		return move;
+	}
+	public JPanel getYutPanel(int move) {
+		return new YutPanel(move) {
+			public void paintComponent(Graphics g) {
+				g.drawImage(image.getImage(),0,0,null);
+			}
+		};
+	}
+
+}
+
+class YutMotionPanel extends JPanel{
+	
+	Image image;
+	
+	YutMotionPanel() {
+		image = Toolkit.getDefaultToolkit().createImage("../image/yut_motion.gif");  
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (image != null) {
+			g.drawImage(image, 0, 0, this);
+		}
+	}
+}
+
+class YutPanel extends JPanel{
+	
+	ImageIcon image;
+	JLabel yutImage;
+	
+	public YutPanel(int move) {
+		setLayout(null);
+		setBounds(0, 0, 1, 1);
+		
+		image = new ImageIcon("../image/" + move + ".PNG");
+		yutImage = new JLabel(image);
+		add(yutImage);
+	}
 }
